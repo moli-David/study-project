34a2753d38ef0565c8607b906caf134a8550bfb1
David <17078387@qq.com>
2023-07-24 17:11:32 +0800

    配置登录验证接口返回JSON数据

--- syudy-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
+++ syudy-project-backend/src/main/java/com/example/config/SecurityConfiguration.java
@@ -12,13 +12,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
